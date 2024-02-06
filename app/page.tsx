e1431c9ff04e631cd00dfc966f91bf3f24faeac2
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-02-07 00:42:26 +0200

    style: add basic CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 export default function Home() {
   return (
-    <main>
-      <section>
-        <header>Your list of tasks</header>
-        <ul>
-          <li>
+    <main className="h-screen bg-red-200 py-5 px-10 ">
+      <section className="flex justify-center flex-wrap flex-col items-center gap-10">
+        <header className="text-center uppercase">Your list of tasks</header>
+        <ul className="w-1/2 flex flex-col gap-4">
+          <li className="flex gap-4">
             <input type="checkbox" />
-            <p>do thing #1</p>
+            <p>Do task #1</p>
           </li>
-          <li>
+          <li className="flex gap-4">
             <input type="checkbox" />
-            <p>do thing #1</p>
+            <p>Do task #2</p>
           </li>
-          <li>
+          <li className="flex gap-4">
             <input type="checkbox" />
-            <p>do thing #1</p>
+            <p>Do task #3</p>
           </li>
         </ul>
       </section>
