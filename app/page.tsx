4b178fa4ecd934880e1999c8e9a33df337223602
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-02-07 23:01:07 +0200

    feat: add state for tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
+"use client";
+import { useState } from "react";
+
+function Task({taskDescription}:{taskDescription: string}) {
+  return (
+    <li className="flex gap-4">
+      <input type="checkbox" />
+      <p>{taskDescription}</p>
+    </li>
+  );
+}
+
 export default function Home() {
+  const [tasks, setTasks] = useState(["task 1", "task 2", "task 3"]);
+
   return (
     <main className="h-screen bg-red-200 py-5 px-10 ">
       <section className="flex justify-center flex-wrap flex-col items-center gap-10">
         <header className="text-center uppercase">Your list of tasks</header>
         <ul className="w-1/2 flex flex-col gap-4">
-          <li className="flex gap-4">
-            <input type="checkbox" />
-            <p>Do task #1</p>
-          </li>
-          <li className="flex gap-4">
-            <input type="checkbox" />
-            <p>Do task #2</p>
-          </li>
-          <li className="flex gap-4">
-            <input type="checkbox" />
-            <p>Do task #3</p>
-          </li>
+          {tasks.map(task => <Task taskDescription={task} key={task} />)}
         </ul>
       </section>
     </main>
