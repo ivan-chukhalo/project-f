7749c4e8384819729d53a82f8f390ddc9419ae42
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-02-12 22:45:09 +0200

    feat: complited tasks has an appropriate slyle and state now

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,11 @@ function Task({
   isActive: boolean;
 }) {
   const inputId = nanoid();
+  const [isChecked, setIsChecked] = useState(isActive)
   return (
     <li className="flex gap-4 text-lg">
-      <input type="checkbox" id={inputId} />
-      <label htmlFor={inputId}>{taskDescription}</label>
+      <input type="checkbox" id={inputId} onClick={()=>setIsChecked(!isChecked)}/>
+      <label htmlFor={inputId} className={isChecked ? "" : "line-through text-gray-500"}>{taskDescription}</label>
     </li>
   );
 }
