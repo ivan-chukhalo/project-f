d07da6dfcc32928675bea4426eb13dc0975a9125
Ivan Chukhalo <ivanchukhalo@gmail.com>
2024-02-12 22:21:34 +0200

    refactor: use prettier on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import { useState } from "react";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 
-function Task({taskDescription, isActive}: {taskDescription: string, isActive: boolean }) {
+function Task({
+  taskDescription,
+  isActive,
+}: {
+  taskDescription: string;
+  isActive: boolean;
+}) {
   const inputId = nanoid();
   return (
     <li className="flex gap-4 text-lg">
@@ -13,20 +19,26 @@ function Task({taskDescription, isActive}: {taskDescription: string, isActive: b
 }
 
 export default function Home() {
-  const [tasks, setTasks] = useState(
-    [
-      {taskDescription: "task 1", isActive: true},
-      {taskDescription: "task 2", isActive: true},
-      {taskDescription: "task 3", isActive: true},
-    ]
-  );
+  const [tasks, setTasks] = useState([
+    { taskDescription: "task 1", isActive: true },
+    { taskDescription: "task 2", isActive: true },
+    { taskDescription: "task 3", isActive: true },
+  ]);
 
   return (
     <main className="h-screen bg-red-200 py-5 px-10 ">
       <section className="flex justify-center flex-wrap flex-col items-center gap-10">
-        <header className="text-center uppercase text-2xl">Your list of tasks</header>
+        <header className="text-center uppercase text-2xl">
+          Your list of tasks
+        </header>
         <ul className="w-1/2 flex flex-col gap-4">
-          {tasks.map(currentTask => <Task taskDescription={currentTask.taskDescription} isActive={currentTask.isActive} key={nanoid()} />)}
+          {tasks.map((currentTask) => (
+            <Task
+              taskDescription={currentTask.taskDescription}
+              isActive={currentTask.isActive}
+              key={nanoid()}
+            />
+          ))}
         </ul>
       </section>
     </main>
